f1485d7d431f97c29b137627f9767bb1281e5dde
lioneljuru <lioneljuru@gmail.com>
2023-07-20 17:17:33 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - test if the number is positive or negative
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
