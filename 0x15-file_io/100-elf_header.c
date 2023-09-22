fdb824457322ad7cac2f23949420d8589fbcae9b
jumacol <jurumacol@gmail.com>
2023-09-22 17:33:42 +0300

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,8 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <elf.h>
 /**
  * print_error - prints out the error message
  * @message: the error message
