03ff2767dbb237b84d2091f593e8f520e198acd9
jumacol <jurumacol@gmail.com>
2023-10-30 15:24:51 +0200

    fileio

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * error_exit - prints out error messages
+ * @message: the error to print
+ *
+ * Return: void
+ */
+void error_exit(const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * main - copies content of a file to another
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from = 0;
+	int fd_to = 0;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+	
+	if (argc != 3)
+	{
+		error_exit("Usage: cp file_from file_to");
+	}
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+			| S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	exit(EXIT_SUCCESS);
+
+	return (0);
+}
